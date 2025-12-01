e5c802d1d9a5f5e8de007ac6e7331d72c9102ca4
Nathan Zimniak <nathan.zimniak@protonmail.com>
2025-12-01 15:34:47 +0100

    add new three body preset

--- init.py
+++ init.py
@@ -30,7 +30,7 @@ def earth_sun():
     point_edge_widths = [15/5, 6/5]
     trail_widths      = [15/3, 6/3]
     axis_limits       = [-2.0, 2.0, -2.0, 2.0, -2.0, 2.0]
-    elevation_angle   = 35
+    elevation_angle   = 40
     azimuthal_angle   = 45
 
     return {"t_ini":             t_ini,
@@ -63,11 +63,11 @@ def inner_solar_system():
     M_venus   = 2.45e-6
     M_earth   = 3.00e-6
     M_mars    = 3.23e-7
-    masses = [M_sun,
-              M_mercury,
-              M_venus,
-              M_earth,
-              M_mars]
+    masses    = [M_sun,
+                 M_mercury,
+                 M_venus,
+                 M_earth,
+                 M_mars]
 
     # Semi-major axes (in AU)
     a_mercury = 0.39
@@ -81,10 +81,10 @@ def inner_solar_system():
                  [a_mars,    0.0, 0.0]]
 
     # Velocities for circular orbits (in AU/year)
-    v_mercury = 2*math.pi/math.sqrt(a_mercury)
-    v_venus   = 2*math.pi/math.sqrt(a_venus)
-    v_earth   = 2*math.pi/math.sqrt(a_earth)
-    v_mars    = 2*math.pi/math.sqrt(a_mars)
+    v_mercury  = 2*math.pi/math.sqrt(a_mercury)
+    v_venus    = 2*math.pi/math.sqrt(a_venus)
+    v_earth    = 2*math.pi/math.sqrt(a_earth)
+    v_mars     = 2*math.pi/math.sqrt(a_mars)
     velocities = [[0.0, 0.0, 0.0],
                   [0.0,  v_mercury, 0.0],
                   [0.0,  v_venus, 0.0],
@@ -119,61 +119,35 @@ def inner_solar_system():
             "elevation_angle":   elevation_angle,
             "azimuthal_angle":   azimuthal_angle}
 
-
-def three_body_choreography():
+def three_body_3d_orbits():
     # Time parameters
     t_ini   = 0.0
-    t_end   = 20.0           # Simulate 20 years
-    N_steps = int(1000) # 1 step per 1 days
+    t_end   = 20.0           # durée suffisante pour voir plusieurs périodes
+    N_steps = int(2000)      # à adapter si tu veux plus/moins de résolution
 
-    # Masses (in solar masses)
-    M_1 = 1.0
-    M_2 = 1.0
-    M_3 = 1.0
-    masses = [M_1,
-              M_2,
-              M_3]
+    # Masses (toutes égales, unités arbitraires cohérentes)
+    M1 = 1.0
+    M2 = 1.0
+    M3 = 1.0
+    masses = [M1, M2, M3]
 
-    # Semi-major axes (in AU)
-    a_mercury = 0.39
-    a_venus   = 0.723
-    a_earth   = 1.0
-    a_mars    = 1.524
-    positions = [[0.0, 0.0, 0.0],
-                 [a_mercury, 0.0, 0.0],
-                 [a_venus,   0.0, 0.0],
-                 [a_earth,   0.0, 0.0],
-                 [a_mars,    0.0, 0.0]]
-
-    # Initial positions (planar figure-eight solution, z = 0)
-    x1 =  0.97000436
-    y1 = -0.24308753
-    z1 = 0.0
-    x2 = -0.97000436
-    y2 =  0.24308753
-    z2 = 0.0
-    x3 =  0.0
-    y3 =  0.0
-    z3 = 0.0
+    # Positions initiales (tes x, avec z = 0)
+    x1, y1, z1 = -1.0, 0.0, 0.0
+    x2, y2, z2 =  1.0, 0.0, 0.0
+    x3, y3, z3 =  0.0, 0.0, 0.0
     positions = [[x1, y1, z1],
                  [x2, y2, z2],
                  [x3, y3, z3]]
 
-    # Initial velocities (figure-eight solution, z = 0)
-    vx1 =  0.4662036850
-    vy1 =  0.4323657300
-    vz1 = 0.0
-    vx2 =  0.4662036850
-    vy2 =  0.4323657300
-    vz2 = 0.0
-    vx3 = -0.93240737
-    vy3 = -0.86473146
-    vz3 = 0.0
+    # Vitesses initiales (tes v, avec vz = 0)
+    vx1, vy1, vz1 =  0.464445,  0.39606, 0.0
+    vx2, vy2, vz2 =  0.464445,  0.39606, 0.0
+    vx3, vy3, vz3 = -0.92889,  -0.79212, 0.0
     velocities = [[vx1, vy1, vz1],
                   [vx2, vy2, vz2],
                   [vx3, vy3, vz3]]
 
-
+    # Paramètres visuels
     trail_length      = 400
     point_colors      = ["#FF0000", "#00AA00", "#0000FF"]
     point_edge_colors = ["#FF0000", "#00AA00", "#0000FF"]
@@ -181,7 +155,8 @@ def three_body_choreography():
     point_sizes       = [10, 10, 10]
     point_edge_widths = [size/5 for size in point_sizes]
     trail_widths      = [size/3 for size in point_sizes]
-    axis_limits       = [-1.5, 1.5, -1.5, 1.5, -0.5, 0.5]
+
+    axis_limits       = [-2.0, 2.0, -2.0, 2.0, -1.0, 1.0]
     elevation_angle   = 40
     azimuthal_angle   = 45
 
@@ -203,11 +178,16 @@ def three_body_choreography():
             "azimuthal_angle":   azimuthal_angle}
 
 
+
+
+
+
+
 # ----- Sélecteur de preset -----
 
 AVAILABLE_PRESETS = {"earth_sun": earth_sun,
                      "inner_solar_system": inner_solar_system,
-                     "three_body_choreography": three_body_choreography}
+                     "three_body_3d_orbits": three_body_3d_orbits}
 
 
 def load_preset(name="default"):
