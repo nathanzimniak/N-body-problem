eaadc848e27015aa28caf1ef837fee1635d7caf9
Nathan Zimniak <nathan.zimniak@protonmail.com>
2025-12-03 20:01:16 +0100

    test

--- src/init.py
+++ src/init.py
@@ -1,39 +1,9 @@
 import math
 
-def earth_sun():
-    # Time parameters
-    t_ini   = 0.0
-    t_end   = 2.0            # Simulate 2 years
-    N_steps = int(365*t_end) # 1 step per day
-
-    # Masses (in solar masses)
-    M_sun     = 1.0
-    M_earth   = 3.00e-6
-    masses = [M_sun,
-              M_earth]
-
-    # Semi-major axes (in AU)
-    a_earth   = 1.0
-    positions = [[0.0, 0.0, 0.0],
-                 [a_earth, 0.0, 0.0]]
-
-    # Velocities for circular orbits (in AU/year)
-    v_earth   = 2*math.pi/math.sqrt(a_earth)
-    velocities = [[0.0, 0.0, 0.0],
-                  [0.0,  v_earth, 0.0]]
-
-    return {"t_ini":             t_ini,
-            "t_end":             t_end,
-            "N_steps":           N_steps,
-            "masses":            masses,
-            "positions":         positions,
-            "velocities":        velocities}
-
-
 def inner_solar_system():
     # Time parameters
     t_ini   = 0.0
-    t_end   = 1.0            # Simulate 1 year
+    t_end   = 2.0            # Simulate 2 years
     N_steps = int(365*t_end) # 1 step per 1 days
 
     # Masses (in solar masses)
@@ -77,19 +47,19 @@ def inner_solar_system():
             "positions":         positions,
             "velocities":        velocities}
 
-def three_body_3d_orbits():
+def three_body_orbits():
     # Time parameters
     t_ini   = 0.0
-    t_end   = 20.0           # durée suffisante pour voir plusieurs périodes
-    N_steps = int(2000)      # à adapter si tu veux plus/moins de résolution
+    t_end   = 2.0            # Simulate 2 years
+    N_steps = int(365*t_end*2) # 1 step per 1 days
 
-    # Masses (toutes égales, unités arbitraires cohérentes)
+    # Masses (in solar masses)
     M1 = 1.0
     M2 = 1.0
     M3 = 1.0
     masses = [M1, M2, M3]
 
-    # Positions initiales (tes x, avec z = 0)
+    # Initial positions
     x1, y1, z1 = -1.0, 0.0, 0.0
     x2, y2, z2 =  1.0, 0.0, 0.0
     x3, y3, z3 =  0.0, 0.0, 0.0
@@ -97,10 +67,10 @@ def three_body_3d_orbits():
                  [x2, y2, z2],
                  [x3, y3, z3]]
 
-    # Vitesses initiales (tes v, avec vz = 0)
-    vx1, vy1, vz1 = 0.464445,  0.39606, 0.0
-    vx2, vy2, vz2 = 0.464445,  0.39606, 0.0
-    vx3, vy3, vz3 = -0.92889,  -0.79212, 0.0
+    # Initial velocities
+    vx1, vy1, vz1 = 2*math.pi*0.464445,  2*math.pi*0.39606, 0.0
+    vx2, vy2, vz2 = 2*math.pi*0.464445,  2*math.pi*0.39606, 0.0
+    vx3, vy3, vz3 = -2*math.pi*0.92889,  -2*math.pi*0.79212, 0.0
     velocities = [[vx1, vy1, vz1],
                   [vx2, vy2, vz2],
                   [vx3, vy3, vz3]]
@@ -120,9 +90,8 @@ def three_body_3d_orbits():
 
 # ----- Sélecteur de preset -----
 
-AVAILABLE_PRESETS = {"earth_sun": earth_sun,
-                     "inner_solar_system": inner_solar_system,
-                     "three_body_3d_orbits": three_body_3d_orbits}
+AVAILABLE_PRESETS = {"inner_solar_system": inner_solar_system,
+                     "three_body_orbits": three_body_orbits}
 
 
 def load_preset(name="default"):
