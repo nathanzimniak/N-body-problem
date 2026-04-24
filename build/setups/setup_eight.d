3b59fb087ced9dbaf67f105bc862e8631ac08496
Nathan Zimniak <78742247+nathanzimniak@users.noreply.github.com>
2026-04-24 02:26:16 +0200

    Exclude generated build artifacts from GitHub linguist stats

--- build/setups/setup_eight.d
+++
@@ -1,5 +0,0 @@
-build/setups/setup_eight.o: setups/setup_eight.cpp setups/setups.hpp \
- src/solver.hpp src/numerics/time_integrators.hpp
-setups/setups.hpp:
-src/solver.hpp:
-src/numerics/time_integrators.hpp:
